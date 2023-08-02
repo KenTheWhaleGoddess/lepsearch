f980697de712837f172c5a082421b051c00e080a
KenTheWhaleGoddess <kenpreiser@gmail.com>
2023-08-01 19:14:57 -0700

    basic script

---
+++ index.js
@@ -0,0 +1,52 @@
+import axios from "axios";
+import 'dotenv/config';
+import * as fs from 'fs';
+
+const first = await axios.get(
+  `https://api.opensea.io/v2/collection/low-effort-punks/nfts`,
+  { headers: { "X-API-KEY": process.env.OS_KEY } }
+);
+let punks = first.data.nfts;
+let next = first.data.next;
+let done = false;
+while (!done) {
+    const data = await axios.get(
+      `https://api.opensea.io/v2/collection/low-effort-punks/nfts?next=` + next,
+      { headers: { "X-API-KEY": process.env.OS_KEY } }
+    );
+    console.log(data.data);
+    punks = punks.concat(data.data.nfts);
+    if ('next' in data.data) {
+        next  = data.data.next;
+    } else {
+      done = true;
+    }
+  }
+
+let punkData = punks.map((punk) => (
+  {
+    "identifier": punk.identifier,
+    "name": punk.name,
+    "image": punk.image_url,
+    "created_at": punk.created_at,
+    "description": punk.description
+  })
+);
+
+for(punk in punkData) {
+
+  fs.writeFile('./'+ punk['identifier'] +'.json', JSON.stringify(punk), err => {
+    if (err) {
+      console.error(err);
+    }
+    // file written successfully
+  });
+}
+
+
+fs.writeFile('./results.json', JSON.stringify(punkData), err => {
+  if (err) {
+    console.error(err);
+  }
+  // file written successfully
+});
